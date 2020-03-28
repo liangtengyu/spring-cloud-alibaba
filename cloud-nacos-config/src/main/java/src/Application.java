de138029742df410db34c52a21d4efdd1a989139
lty <srevers@vip.qq.com>
2020-03-28 12:57:09 +0800

    配置config-server 启用nacos为配置中心

--- cloud-nacos-config/src/main/java/src/Application.java
+++ cloud-nacos-config/src/main/java/src/Application.java
@@ -3,6 +3,7 @@ package src;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
        SpringApplication.run(Application.class,args);
